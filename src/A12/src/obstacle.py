00c82691053f5b2227bac09b5743fc9d5c09614b
linussuni <ziegert.l@goolgemail.com>
2019-02-06 19:55:15 +0100

    obstacle: working, discovering obstacles on lane

--- src/A12/src/obstacle.py
+++ src/A12/src/obstacle.py
@@ -8,6 +8,7 @@ import math
 import subprocess
 import numpy as np
 from std_msgs.msg import Int16
+from std_msgs.msg import Int8
 from PIL import Image
 from PIL import ImageDraw
 from sensor_msgs.msg import LaserScan
@@ -41,7 +42,7 @@ def scan_callback(data):
 	global scanner_list
 	scanner_list = []
 	# in relev_d ist kein fluessiger Uebergang dies wird
-	# in den weiteren Rechnungen aber berücksichtigt
+	# in den weiteren Rechnungen aber beruecksichtigt
 	# inf -> 0
 	# alles ueber 1,5m -> 0
 	relev_d = distances[0:45] + distances[314:359]
@@ -49,10 +50,9 @@ def scan_callback(data):
 		if x >=1.3:
 			scanner_list.append(0)
 		else:
-			scanner_list.append(x-0.2)
+			scanner_list.append(x+0.1)
 
-def findpoint(point,laneID): #Finde den nahsten Punkt auf einer bestimmen Spur
-
+def findPoint(point,laneID): #Finde den nahsten Punkt auf einer bestimmen Spur
 	x= point[0]
 	y= point[1]
 	# Distanz vom inneren Oval festlegen
@@ -95,18 +95,21 @@ def transLidarInf(A, currPos, orient):  # Lidar-Information in Koordinaten ueber
 	C = [] #Enthaelt nach dem for-loop die Richtung in RAD aller Lidar-Informationen, passend zu A
 	for i in range(0, len(A)):
 		if i < len(A) / 2:
-			C.append(orient + i * incr)
+			C.append(orient - i * incr)
 		else:
-			C.append(orient + -(len(A) - i) * incr)
+			C.append(orient + (len(A) - i) * incr)
 		if C[i] > math.pi:
 			C[i] = -2 * math.pi + C[i]
+		elif C[i] < (-(math.pi)):
+			C[i] = 2 * math.pi - C[i]
 	D = []
 	for i in range(0, len(C)):
-		#x/y-Offset kann durch den Winkel Beta (C[i]) und die Länge der Hypotenuse (A[i]) berechnet werden
-		xadd = round(A[i] * 100 * math.cos(C[i]), 2)
+		#x/y-Offset kann durch den Winkel Beta (C[i]) und die Laenge der Hypotenuse (A[i]) berechnet werden
+		xadd = A[i] * 100 * math.cos(C[i])
 		if abs(C[i]) >= math.pi / 2:
 			xadd = -xadd
-		yadd = round(math.sqrt((A[i] * 100) ** 2 - xadd ** 2), 2)
+		yadd = round(math.sqrt(((A[i] * 100) ** 2) - (xadd ** 2)), 2)
+		xadd = round(xadd,2)
 		if A[i] == 0: # Nullen in A bedeuten, dass kein Objekt gefunden wurde, werden also aussortiert
 			continue
 		if 0 <= C[i] < math.pi / 2:#Je nachdem, in welche Richtung der Winkel zeigt, muss der Offset addiert oder subtrahiert werden
@@ -123,32 +126,33 @@ def transLidarInf(A, currPos, orient):  # Lidar-Information in Koordinaten ueber
 def findRelevantObstacles(A,laneID): #Pruefen, ob es auf einer Bahn Hindernisse gibt
 	ret = []
 	for i in A:
-		ref = findpoint(i,laneID)#Finde den Referenzpunkt auf der Bahn
+		ref = findPoint(i,laneID)#Finde den Referenzpunkt auf der Bahn
 		dist = math.sqrt((ref[0]-i[0])**2 + (ref[1]-i[1])**2) #Berechne die Distanz zwischen dem Punkt und dem Referenz-Punkt
-		if dist <=15:#Falls der Punkt maximal 15 Zentimeter vom Zentrum der Bahn entfernt ist, ist das Obstacle relevant
+		if dist <=13:#Falls der Punkt maximal 15 Zentimeter vom Zentrum der Bahn entfernt ist, ist das Obstacle relevant
 			ret.append(ref)
 	return (ret)
 
 
 def closestObstacles(currPos,laneID,A): #Das nahste Hinderniss auf einer Bahn finden
-  distances = []
+	distances = []
 	for i in A:
 		dist = findDistance(currPos,i,laneID,0)#Berechne die Distanz vom Auto zu den relevanten Obstacles
 		distances.append(dist)
-  if len(distances)>2: #Um false-positives entgegenzuwirken
-    return (distances)
-  else:
-    return ([100]) #Also keine relevanten Obstacles
+	if len(distances)>2: #Um false-positives entgegenzuwirken
+		return (distances)
+	else:
+		return ([100]) #Also keine relevanten Obstacles
 
 
 def findDistance(point1,point2,laneID,distance):#Distanz zwischen 2 Punkten auf der
 	#Bahn finden, wobei point1 vor point2 liegen muss.
+	point1=findPoint(point1,laneID)
 	x1=point1[0]
 	y1=point1[1]
 	x2=point2[0]
 	y2=point2[1]
 
-	if laneID == 0: #Radius der Kreise, basieren auf der Bahn festlegen
+	if laneID == 0: #Radius der Kreise, basierend auf der Bahn festlegen
 		rad = 135
 	else:
 		rad = 165
@@ -156,14 +160,14 @@ def findDistance(point1,point2,laneID,distance):#Distanz zwischen 2 Punkten auf 
 	totUmf = rad*math.pi #Umfang eines Halbkreises
 
 	if y1 <= 215: #Den Winkel von Punkt 1 auf dem Halbkreis berechnen (nutzlos, falls Punkt 1 auf keinem Halbkreis liegt.)
-		beta = 90+math.degrees(math.asin((215-y1)/rad))
+		beta = 90+(math.degrees(math.asin((215.0-y1)/rad)))
 	else:
-		beta = 90-math.degrees(math.asin((y1-215)/rad))
+		beta = 90-(math.degrees(math.asin((y1-215.0)/rad)))
 
 	if y2 <= 215: #Den Winkel von Punkt 2 auf dem Halbkreis berechnen (nutzlos, falls Punkt 2 auf keinem Halbkreis liegt.)
-		alpha = 90+math.degrees(math.asin((215-y2)/rad))
+		alpha = 90+math.degrees(math.asin((215.0-y2)/rad))
 	else:
-		alpha = 90-math.degrees(math.asin((y2-215)/rad))
+		alpha = 90-math.degrees(math.asin((y2-215.0)/rad))
 
 	if x1 <= 194: #oberer Halbkreis
 		umf = totUmf-(beta/180)*totUmf #Restumfang von Punkt 1 bis zum Ende des oberen Halbkreises
@@ -176,7 +180,7 @@ def findDistance(point1,point2,laneID,distance):#Distanz zwischen 2 Punkten auf 
 			newPoint1=point2
 
 	elif 195 <= x1 <= 405 and y1 <= 215: #linke Gerade
-	    	if 195 > x2 or x2 > 405 or y2 > 215: #Falls sich der 2.Punkt nich im gleichen Bereich befindet
+	    	if 195 > x2 or x2 > 405 or y2 > 215: #Falls sich der 2.Punkt nicht im gleichen Bereich befindet
 			newPoint1 = (406,215-rad)
 			distance += 406-x1
 		else:
@@ -184,7 +188,7 @@ def findDistance(point1,point2,laneID,distance):#Distanz zwischen 2 Punkten auf 
 			distance += x2-x1
 
 	elif 195 <= x1 <= 405 and 216 <= y1: #rechte Gerade
-		if 105 > x2 or x2 > 405 or y2 < 216: #Falls sich der 2.Punkt nich im gleichen Bereich befindet
+		if 105 > x2 or x2 > 405 or y2 < 216: #Falls sich der 2.Punkt nicht im gleichen Bereich befindet
 			newPoint1 = (194,215+rad)
 			distance += x1-194
 		else:
@@ -192,8 +196,8 @@ def findDistance(point1,point2,laneID,distance):#Distanz zwischen 2 Punkten auf 
 			distance += x1-x2
 
 	elif 406 <= x1: #unterer Halbkreis
-		umf = totUmf-(beta/180)*totUmf #Restumfang von Punkt 1 bis zum Ende des unteren Halbkreises
-		if x2 < 406: #Falls sich der 2.Punkt nich im gleichen Bereich befindet
+		umf = (beta/180)*totUmf #Restumfang von Punkt 1 bis zum Ende des unteren Halbkreises
+		if x2 < 406: #Falls sich der 2.Punkt nicht im gleichen Bereich befindet
 			distance += umf
 			newPoint1 = (405,215+rad)
 		else:
@@ -201,12 +205,19 @@ def findDistance(point1,point2,laneID,distance):#Distanz zwischen 2 Punkten auf 
 			distance += umf-umf2
 			newPoint1=point2
 
-	if newPoint1 != point2: #Wenn der neue berechnete Punkt nicht mit Punkt 2 übereinstimmt, wird die Funktion erneut aufgerufen,
-				#mit dem neuen Punkt als Punkt 1 und der bereits "zurückgelegten" Distanz (Punkt 1 bis neuer Punkt 1)
+	if newPoint1 != point2: #Wenn der neue berechnete Punkt nicht mit Punkt 2 uebereinstimmt, wird die Funktion erneut aufgerufen,
+				#mit dem neuen Punkt als Punkt 1 und der bereits "zurueckgelegten" Distanz (Punkt 1 bis neuer Punkt 1)
 		return (findDistance(newPoint1,point2,laneID,distance))
 	else:
 		return (int(distance)) #Wenn newPoint1 == Point 2, wurde die richtige Distanz gefunden
 
+""" Hier fuehren wir eine neue Logik ein:
+100 : lane 0 und 1 sind frei
+101 : lane 0 ist frei lane 1 nicht
+110 : lane 0 ist nicht frei lane 1 ist frei
+111 : keine lane frei
+Wobei lane 0 die innere und 1 die aeussere ist
+"""
 
 def obstacle_on_lane():
 	global scanner_list
@@ -215,35 +226,38 @@ def obstacle_on_lane():
 	pub = rospy.Publisher('/obstacle_on_lane', Int8, queue_size=10)
 	rospy.init_node('obstacles', anonymous=True)
 	rate = rospy.Rate(10)
-  while not rospy.is_shutdown():
-    ob_int = 100 # Hier fuehren wir eine neue Logik ein:
-		# 100 : lane 0 und 1 sind frei
-    # 101 : lane 0 ist frei lane 1 nicht
-    # 110 : lane 0 ist nicht frei lane 1 ist frei
-    # 111 : keine lane frei
-    # Wobei lane 0 die innere und 1 die aeussere ist
-    obstacleList=transLidarInf(scanner_list, global_curPos, global_orientation) #Fetched die Lidar-Informationen
+	while not rospy.is_shutdown():
+		ob_int = 100
+		obstacleList=transLidarInf(scanner_list, global_curPos, global_orientation) #Fetched die Lidar-Informationen
 
-    obstacles0 = closestObstacles(findPoint(global_curPos,0),0,findRelevantObstacles(obstacleList,0)) #Nahstes Obstacle auf Bahn 0
-    obstacles1 = closestObstacles(findPoint(global_curPos,1),1,findRelevantObstacles(obstacleList,1)) #Nahstes Obstacle auf Bahn 1
-    if (min(obstacles0) < 100) and (min(obstacles1) < 100): #Wenn auf beiden Bahnen relevante Hindernisse sind
-      ob_int = 111
-    elif (min(obstacles0) < 100): #Wenn nur auf Bahn 0 Hindernisse sind
-      ob_int = 110
-    elif (min(obstacles1) < 100): #Wenn nur auf Bahn 1 Hindernisse sind
-      ob_int = 101
-    else: #Wenn es keinerlei relevante Hindernisse gibt
-      ob_int = 100
-    rospy.loginfo(ob_int)
-    pub.publish(ob_int)
-    rate.sleep()
+		obstacles0 = closestObstacles(findPoint(global_curPos,0),0,findRelevantObstacles(obstacleList,0)) #Nahstes Obstacle auf Bahn 0
+		obstacles1 = closestObstacles(findPoint(global_curPos,1),1,findRelevantObstacles(obstacleList,1)) #Nahstes Obstacle auf Bahn 1
+		if (min(obstacles0) < 100) and (min(obstacles1) < 100): #Wenn auf beiden Bahnen relevante Hindernisse sind
+			ob_int = 111
+		elif (min(obstacles0) < 100): #Wenn nur auf Bahn 0 Hindernisse sind
+			ob_int = 110
+		elif (min(obstacles1) < 100): #Wenn nur auf Bahn 1 Hindernisse sind
+			ob_int = 101
+		else: #Wenn es keinerlei relevante Hindernisse gibt
+			ob_int = 100
+		#print ("orientation:",global_orientation)
+		#print ("scanner_list:",scanner_list)
+		#print ("global_curPos:",global_curPos)
+		#print ("Destination:", findPoint((400,357),0))
+		#print ("Distance:",findDistance(global_curPos,findPoint((400,357),0),0,0))
+		#print ("obstacleList:",obstacleList,"len:",len(obstacleList))
+		#print ("relObs0:", findRelevantObstacles(obstacleList,0))
+		#print ("relObs1:", findRelevantObstacles(obstacleList,1))
+		rospy.loginfo(ob_int)
+		pub.publish(ob_int)
+		rate.sleep()
 
 sub_scan = rospy.Subscriber("/scan", LaserScan, scan_callback)
-sub_odom = rospy.Subscriber("/localization/odom/5",Odometry, odom_callback)
+sub_odom = rospy.Subscriber("/localization/odom/12",Odometry, odom_callback)
 
 if __name__ == '__main__':
-  try:
+	try:
 		obstacle_on_lane()
-	except rospy.ROSInterruprExeption as err:
-    print(err)
+	except rospy.ROSInterruptException as err:
+		print(err)
 
